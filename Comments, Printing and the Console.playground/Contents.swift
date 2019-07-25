8acbfc6dc06f6b034c054c23a23b6e399b79d395
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-07-25 14:38:00 -0400

    init commit

---
+++ Comments, Printing and the Console.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit  //User Interface Kit, code created already so we don't have to code certain things
+// just imports the package of code that was already created
+
+var str = "Hello, playground"
+
+// Comments are created with two slashes "//" before the statement
+// Helps describe what the following lines of code are doing
+
+/*
+ Too make multiple lines of comments or to comment out multiple lines,
+ start with "/*" and end with "*/"
+ */
+
+var monsterHealth = 19 //You can leave space when assign variables with "=","+","/","*"
+//Also you don't need to put spaces but must be the same on both sides
+// Examples: variable=10, variable = 10
+monsterHealth = monsterHealth + 22
+monsterHealth = monsterHealth / 2
+// Playground helps keep track of the variables to the right pane, but in regular Xcode, it doesn't show
+print(monsterHealth) // this can help print out to the console
+print("Hello World")

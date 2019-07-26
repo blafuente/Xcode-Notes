00e4631e0e7ebeba218b9828a9fc8f5313f1e22a
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-07-26 15:52:34 -0400

    bottles of beer loop

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -12,12 +12,37 @@ import UIKit
 //print(sum)
 
 // printing out a number within the range(1 to 10)
-for number in 1...10 {
-    print(number)
+//for number in 1...10 {
+//    print(number)
+//}
+//for number in 1..<10 {
+//    print(number)
+//}
+//for number in 1..<10 where number % 2 == 0{
+//    print(number)
+//}
+
+//var startingBeers = 99
+//for beers in 1...99 {
+////    startingBeers -= 1
+//    print("There's \(startingBeers) bottles of beer on the wall, \(startingBeers) bottles of beer. Take one down and pass it around, \(startingBeers -= 1) bottles of beer on the wall.")
+//    print(beers)
+//}
+
+// for a new line when printing the string, use "\n"
+
+//Solution
+func beerSong(bottles: Int) -> String{
+    var lyrics: String = ""
+    
+    for number in (1...bottles).reversed() {
+        let newLine: String = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. Take one down and pass it around, \(number - 1) bottles of beer on the wall.\n"
+        lyrics += newLine
+        //print(lyrics)
+    }
+    
+    lyrics += "\nNo more bottles of beer on the wall, no more more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    return lyrics
 }
-for number in 1..<10 {
-    print(number)
-}
-for number in 1..<10 where number % 2 == 0{
-    print(number)
-}
+
+print(beerSong(bottles: 99))

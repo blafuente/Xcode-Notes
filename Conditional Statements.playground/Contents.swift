f4fa4d2e82fa6a798c9a8b06f9c657ab17e361dc
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-07-26 14:54:15 -0400

    conditionals notes

--- Conditional Statements.playground/Contents.swift
+++ Conditional Statements.playground/Contents.swift
@@ -1,7 +1,31 @@
+//import UIKit
+//
+//func loveCalculator(yourName: String, theirName : String) -> String {
+//
+//    let loveScore = arc4random_uniform(101)
+//
+//    if loveScore > 80 {
+//        return "Your love score is \(loveScore). You love each other like Kanye loves Kanye"
+//    }
+//    else if loveScore > 40 && loveScore <= 80 {
+//        return "Your love score is \(loveScore). You go together like coke and mentos"
+//    }
+//    else{
+//        return "No love possible, you'll be forver alone. Your love score is \(loveScore)"
+//    }
+//
+//    //    return loveScore
+//}
+//
+//print(loveCalculator(yourName: "Brian", theirName: "Gina"))
+
+
 import UIKit
 
-func loveCalculator(yourName: String, theirName : String) -> String {
+//Creating a function that takes 2 inputs and 1 output
+func loveCalculator (yourName : String, theirName : String) -> String {
     
+    //Generating a random number between 0 and 100
     let loveScore = arc4random_uniform(101)
     
     if loveScore > 80 {
@@ -10,11 +34,11 @@ func loveCalculator(yourName: String, theirName : String) -> String {
     else if loveScore > 40 && loveScore <= 80 {
         return "Your love score is \(loveScore). You go together like coke and mentos"
     }
-    else{
-        return "No love possible, you'll be forver alone. Your love score is \(loveScore)"
+    else {
+        return "Your love score is \(loveScore). No love possible, you'll be forever alone!"
     }
     
-//    return loveScore
 }
 
-print(loveCalculator(yourName: "Brian", theirName: "Gina"))
+//Printing the output of the loveCalculator() function
+print(loveCalculator(yourName: "Angela Yu", theirName: "Jack Bauer"))
